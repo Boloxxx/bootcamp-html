e7bb3046456f02b84bcac01e19fabd1146917558
Bolox <76413304+Boloxxx@users.noreply.github.com>
2023-04-28 00:42:05 +0800

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,6 @@
+const familyName = ["Bryan", "Ryan", "Gracia", "Diogenes", "Pamela", "Keilyn", "Bryan", "JavaScript"]
+
+const familyset = new Set(familyName)
+
+console.log(familyset)
+console.log(familyName)
